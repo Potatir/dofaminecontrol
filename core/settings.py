93f627b72a47ddba25bb56e9dedec78c867a53ec
Саша Чурилов <sanyachurilov5@gmail.com>
2025-10-22 07:33:46 +0500

    Fix MEDIA_ROOT path for avatar uploads

--- core/settings.py
+++ core/settings.py
@@ -191,7 +191,7 @@ P1SMS_API_KEY = os.getenv('P1SMS_API_KEY', '')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 # Django REST Framework
 REST_FRAMEWORK = {
